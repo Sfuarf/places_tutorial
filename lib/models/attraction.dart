86c600d26abf6f4809d856e1f6a7c9f22e1fcf77
Sfuarf <s.s.carrick@gmail.com>
2021-08-29 16:50:18 +0800

    add attraction class

---
+++ lib/models/attraction.dart
@@ -0,0 +1,22 @@
+import 'package:places_autocomplete/models/geometry.dart';
+
+class Attraction {
+  final Geometry geometry;
+  final String name;
+  final String address;
+  final double rating;
+
+  Attraction(
+      {required this.geometry,
+      required this.name,
+      required this.address,
+      required this.rating});
+
+  factory Attraction.fromJson(Map<String, dynamic> parsedJson) {
+    return Attraction(
+        geometry: Geometry.fromJson(parsedJson['geometry']),
+        name: parsedJson['name'],
+        address: parsedJson['formatted_address'],
+        rating: parsedJson['rating']);
+  }
+}
